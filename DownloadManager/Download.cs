48bd62b7062c975d5237b8d03f345d946929f4d1
ArtemNim <artem_nim@ukr.net>
2017-08-25 10:08:16 +0300

    Add project files.

---
+++ DownloadManager/Download.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DownloadManager
+{
+    public class Download
+    {
+        public CheckBox IsChecked { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Speed { get; set; }
+        public string Size { get; set; }
+        public string Added { get; set; }
+    }
+}
